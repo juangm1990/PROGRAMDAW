412e481bf6f1f09e5b11cf0f8c0289f9333e6823
juangm1990 <jgm0106@alu.medac.es>
2024-11-25 18:31:37 +0100

    RECURSIVIDAD1

---
+++ primerTrimestre/src/recursividad/recursividadFibonnacci.java
@@ -0,0 +1,18 @@
+public class recursividadFibonnacci {
+
+    public static void main(String[] args) {
+
+        System.out.println(numeroFibonacci(6));
+    }
+
+    public static int numeroFibonacci(int n) {
+        if (n == 0) {
+            return 0; // Caso base: Fibonacci de 0 es 0
+        } else if (n == 1) {
+            return 1; // Caso base: Fibonacci de 1 es 1
+        } else {
+            return numeroFibonacci(n - 1) + numeroFibonacci(n - 2); // Llamada recursiva
+        }
+
+    }
+}
